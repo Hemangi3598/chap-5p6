367ed4319a44792569b440438e6d042986cb642e
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 17:05:04 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,9 @@
+# wapp to read a string and sort the string in alphabetical order
+# amit   aimt 	tina	aint
+
+s1 = input(" enter a string ")	# read the string
+d1 = sorted(s1)
+print(d1)			# use bif sorted()
+ns1 = " ".join(d1)				# from d1 create string using join()
+
+print(ns1)
